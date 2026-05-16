eecd84047ba84a4f573c4a97b5b675608b0b2ecf
deepakkumar <qb-dd@cfgo.com>
2026-05-17 00:05:52 +0530

    Establishing mongoDB connection changes

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 
-app.use('/', (req, res, next)=>{
+app.use('/', (req, res, next) => {
     res.json('Initial setup')
 })
 
-app.listen(8080)
+mongoose.connect('mongodb+srv://Deepakkumar52098:Deepakkumar%402016@cluster0.yxkfr9z.mongodb.net/d-Tracker?appName=Cluster0')
+    .then(result => {
+        console.log("Initialized mongoDB")
+        app.listen(8080)
+    })
+    .catch(err => {
+        console.log(err)
+    })
